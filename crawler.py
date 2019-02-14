dd2d475e6bec51c39fb7378a3f22fd9aa9c05810
LeeKLTW <LeeKLTW@gmail.com>
2019-02-14 19:37:33 +0800

    ssl for https

--- crawler.py
+++ crawler.py
@@ -1,9 +1,13 @@
 # encoding: utf-8
 from urllib.request import urlopen
+# This is for https
+from bs4 import BeautifulSoup
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
-html = urlopen('https://arxiv.org/list/cs.CL/recent')
-html = urlopen('https://redis.io/documentation')
 
-dir(html)
-html.read()
+html = urlopen('https://arxiv.org/list/cs.CL/recent')
+
+
+
+
+
