42929e8be006fae43621170179761b8cf1a47854
LeeKLTW <LeeKLTW@gmail.com>
2019-04-15 19:18:09 +0800

    clean

--- crawler.py
+++ crawler.py
@@ -2,12 +2,8 @@
 from urllib.request import urlopen
 # This is for https
 from bs4 import BeautifulSoup
-import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
+from datetime import datetime
+datetime.now()
 
 html = urlopen('https://arxiv.org/list/cs.CL/recent')
-
-
-
-
 
